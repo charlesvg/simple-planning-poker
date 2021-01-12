535937ccfb785bb75356dcebb9068cbec07d77bd
Charles VG <charles.vangoidsenhoven@gmail.com>
2021-01-12 21:01:18 +0100

    update

--- public/js/socket.js
+++ public/js/socket.js
@@ -1,62 +1,102 @@
 export class Socket {
     constructor() {
-        this.users = [window.poker.username];
+        this.username = window.poker.username;
+        this.users = new Map();
+        this.users.set(this.username, {lastSeen: new Date().getTime()});
         this.refreshUsers();
     }
 
     open() {
-
         const isSSL = window.location.protocol === 'https:';
         this.socket = new WebSocket((isSSL ? "wss://" : "ws://") + window.location.host, "echo-protocol");
         this.socket.onopen = (event) => {
             console.log('Socket open');
-            this.socket.send(JSON.stringify({
-                type: 'USER_JOINED',
-                body: {
-                    username: window.poker.username
-                }
-            }));
+            this.notifyOthersWeJoined();
         }
         this.socket.onmessage = (event) => {
             const msg = JSON.parse(event.data);
             switch (msg.type) {
                 case 'USER_JOINED':
-                    this.handleUserJoined(msg);
+                    this.onUserJoined(msg);
                     break;
                 case 'USER_GREETING':
-                    this.handleUserGreeting(msg);
+                    this.onUserGreeting(msg);
+                    break;
+                case 'HEARTBEAT':
+                    this.onHeartBeat(msg);
                     break;
             }
         }
+
+        setInterval(() => {
+            this.sendHeartBeat();
+            this.checkUsersAreLive();
+        }, 2000);
     }
 
-    handleUserGreeting(msg) {
-        const myUsername = window.poker.username;
-        const hisUsername = msg.body.username;
-        if (myUsername !== hisUsername) {
-            this.users.push(msg.body.username);
+    onHeartBeat(msg) {
+        const remoteUsername = msg.body.username;
+        this.users.get(remoteUsername).lastSeen = this.now();
+    }
+
+    checkUsersAreLive() {
+        let usersToRemove = [];
+        for (let [username, userDetails] of this.users) {
+            if (userDetails.lastSeen + 3000 < this.now()) {
+                usersToRemove.push(username);
+                console.log('User', username, 'timed out');
+            }
+        }
+        usersToRemove.forEach(username => this.users.delete(username));
+        if (usersToRemove.length !== 0) {
             this.refreshUsers();
         }
     }
 
-    handleUserJoined(msg) {
-        const myUsername = window.poker.username;
-        const hisUsername = msg.body.username;
-        if (myUsername !== hisUsername) {
-            this.users.push(hisUsername);
+    sendHeartBeat() {
+        this.send('HEARTBEAT', {
+            username: this.username
+        });
+    }
+
+    notifyOthersWeJoined() {
+        this.send('USER_JOINED', {
+            username: this.username
+        });
+    }
+
+    send(type, content) {
+        this.socket.send(JSON.stringify({
+            type: type,
+            body: content
+        }));
+    }
+
+    now() {
+        return new Date().getTime();
+    }
+
+    onUserGreeting(msg) {
+        const remoteUsername = msg.body.username;
+        if (this.username !== remoteUsername) {
+            this.users.set(remoteUsername, {lastSeen: this.now()});
+            this.refreshUsers();
+        }
+    }
+
+    onUserJoined(msg) {
+        const remoteUsername = msg.body.username;
+        if (this.username !== remoteUsername) {
+            this.users.set(remoteUsername, {lastSeen: this.now()});
             this.refreshUsers();
             this.greetUser();
-            console.log(msg.body.username, 'has joined');
         }
     }
 
     greetUser() {
-        this.socket.send(JSON.stringify({
-            type: 'USER_GREETING',
-            body: {
-                username: window.poker.username
-            }
-        }));
+        this.send('USER_GREETING', {
+            username: this.username
+        });
     }
 
     refreshUsers() {
@@ -67,9 +107,9 @@ export class Socket {
         }
 
         let output = '';
-        this.users.forEach(user => {
-            output += `<div class="col user rounded-top">${user}</div><div class="w-100"></div>`;
-        })
+        for (let [username, userDetails] of this.users) {
+            output += `<div class="col user rounded-top">${username}</div><div class="w-100"></div>`;
+        }
         usersElement.innerHTML = output;
 
     }
